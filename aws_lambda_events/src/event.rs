94c6353dd5c6e610966d397614ca126dd65738cd
David Calavera <david.calavera@gmail.com>
2022-02-17 20:07:01 -0800

    Reexport events (#66)
    * Reexport events in the location they were before.
    
    So people find one less breaking change.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>
    
    * Bump version to a new minor version
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- aws_lambda_events/src/event.rs
+++ aws_lambda_events/src/event.rs
@@ -1,3 +1,18 @@
 // Additional layer of indirection in case we want to make upgrading easy
 // or provide translations in the future.
 pub use super::generated::*;
+
+/// AWS Lambda event definitions for alb.
+#[cfg(feature = "alb")]
+pub use super::alb;
+/// AWS Lambda event definitions for apigw.
+#[cfg(feature = "apigw")]
+pub use super::apigw;
+
+/// CloudWatch Events payload
+#[cfg(feature = "cloudwatch_events")]
+pub use super::cloudwatch_events;
+
+/// AWS Lambda event definitions for dynamodb.
+#[cfg(feature = "dynamodb")]
+pub use super::dynamodb;
